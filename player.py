e8b55b55f9f41698508652a091272363d8f6278a
HiddEnigma <chaoticassembly@hotmail.com>
2022-08-01 16:06:43 -0300

    (Update) - Fix file path for the player's image in player.py.

--- player.py
+++ player.py
@@ -4,5 +4,5 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, position):
         super().__init__()
 
-        self.image = pygame.image.load("../sprites/player.png").convert_alpha()
+        self.image = pygame.image.load("sprites/player.png").convert_alpha()
         self.rect = self.image.get_rect(midbottom=position)
